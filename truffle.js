852ef523f8ffa6b91c914b491a5e58d5151dcaf0
le-dawg <dawid.golebiewski@tuhh.de>
2018-08-10 18:14:19 +0200

    First commit that I would say has reached beta. Looks and behaves the way it should while also having a secure deployment mechanism.

--- truffle.js
+++ truffle.js
@@ -2,23 +2,13 @@ const yargs = require('yargs');
 var provider, address;
 require('dotenv').config();
 //const HDWalletProvider = require("truffle-hdwallet-provider-privkey");
-//var privKeyRopsten = "5da28899b6f5621f34ca8ab5b89c7fe23f8ee43c6f761339fafc57d4091b6167"; // private keys
-var privKeyRopsten = process.env.privKeyRopsten;
 //const provider = new HDWalletProvider(privKeys, "http://localhost:8545");
-const PrivateKeyProvider = require("truffle-privatekey-provider");
-// Not using remote node until secure way to store seed/private key is established.
-//
 // if (yargs.argv.network  == 'ropsten' || yargs.argv.network  == 'mainnet') {
-//   var providerURL = 'http://localhost:8545';
-//   // var providerURL = `https://${yargs.argv.network}.infura.io`
-//   var HDWalletProvider = require("truffle-privatekey-provider");
-//   // todo: Think about more secure way
-//   var mnemonic = yargs.argv.mnemonic;
-//   provider = new HDWalletProvider(mnemonic, providerURL, 0);
-//   // address = "0x" + provider.wallet.getAddress().toString("hex");
-//   // console.log('Provider address', provider.getAddress());
-//   console.log('Deploying to ', providerURL);
-// }
+
+var privKeyRopsten = process.env.privKeyRopsten;
+
+const PrivateKeyProvider = require("truffle-privatekey-provider");
+
 module.exports = {
   networks: {
     development: {
